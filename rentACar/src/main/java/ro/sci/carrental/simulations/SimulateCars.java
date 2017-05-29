0f970bbd33bb3838005645008aba159e2f59655f
Radovici Tudor <radovicitudor@gmail.com>
2017-05-29 16:57:38 +0300

    implemented search by: make make and model

--- rentACar/src/main/java/ro/sci/carrental/simulations/SimulateCars.java
+++ rentACar/src/main/java/ro/sci/carrental/simulations/SimulateCars.java
@@ -1,5 +1,6 @@
 package ro.sci.carrental.simulations;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 import ro.sci.carrental.domain.Car;
 import ro.sci.carrental.repository.CarRepositoryImpl;
 import ro.sci.carrental.repository.CustomerRepositoryImpl;
@@ -12,6 +13,40 @@ import java.util.List;
 
 public class SimulateCars {
     public void searches(CarRepositoryImpl carRepository){
-        System.out.println();
+        System.out.println("List of cars in Repository is :");
+
+        for (Car car: carRepository.getAll())
+        {
+            System.out.println(car.getMake());
+        }
+
+        System.out.println("End of cars in Repository");
+
+
+        //search by make
+
+        CarServiceImpl search1= new CarServiceImpl(carRepository);
+        List<Car> foundCarsByMake= search1.findCarsByMake("BMW");
+
+        System.out.println("Search by make returns:");
+        for (Car car :foundCarsByMake){
+            System.out.println(car.getMake()+" "+car.getModel());
+        }
+
+        System.out.println("End of cars by"+ search1+" make");
+
+        //search by make and model
+
+        CarServiceImpl search2 = new CarServiceImpl(carRepository);
+        List<Car> foundCarsByMakeAndModel = search2.findCarsByMakeAndModel("Ferrari", "458");
+
+        System.out.println("Search by make and model returns:");
+        for(Car car : foundCarsByMakeAndModel){
+            System.out.println(car.getMake()+" "+car.getModel());
+        }
+
+        System.out.println("End of cars by make and model:"+search2);
+
+
     }
 }
