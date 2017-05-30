c496324cd9d48bf49c385ca8c661d8d02ebc3bd2
tudor.radovici <radovicitudor@gmail.com>
2017-05-30 12:33:36 +0300

    implemented RentalCalendar

--- rentACar/src/main/java/ro/sci/carrental/Calendar/RentalCalendar.java
+++ rentACar/src/main/java/ro/sci/carrental/Calendar/RentalCalendar.java
@@ -1,7 +1,24 @@
 package ro.sci.carrental.Calendar;
 
+import ro.sci.carrental.domain.Customer;
+
 /**
  * Created by tudorradovici on 29/05/17.
  */
 public interface RentalCalendar {
+
+    /**
+     *Method simulates a pickup time  for a rented car
+     * @param customer
+     * @param date
+     */
+    void pickupTime(Customer customer, String date);
+
+    /**
+     *Method return a pickup time  for a rented car
+     * @param customer
+     * @param date
+     */
+    void retunrTime(Customer customer, String date);
+
 }
--- rentACar/src/main/java/ro/sci/carrental/Calendar/RentalCalendarImpl.java
+++ rentACar/src/main/java/ro/sci/carrental/Calendar/RentalCalendarImpl.java
@@ -1,7 +1,16 @@
 package ro.sci.carrental.Calendar;
 
+import ro.sci.carrental.domain.Customer;
+
 /**
  * Created by tudorradovici on 29/05/17.
  */
-public class RentalCalendarImpl implements  RentalCalendar{
+public class RentalCalendarImpl implements RentalCalendar{
+    public void pickupTime(Customer customer, String date) {
+
+    }
+
+    public void retunrTime(Customer customer, String date) {
+
+    }
 }
