07d81559fcd9c34079704cf8c5c2f42d90d966ac
tudor.radovici <radovicitudor@gmail.com>
2017-05-31 17:15:05 +0300

    simulate cars not working

---
+++ rentACar/src/main/java/ro/sci/carrental/service/CustomerService.java
@@ -0,0 +1,27 @@
+package ro.sci.carrental.service;
+
+import ro.sci.carrental.domain.Customer;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+/**
+ * Created by tudor.radovici on 31.05.2017.
+ */
+public interface CustomerService {
+
+    /**
+     * Finds customers by first Name
+     * @param firstName
+     * @return List<Customer>
+     */
+    List<Customer> findCustomersByFirstName(String firstName);
+
+    /**
+     * Finds user by date of Birth and date of driver License
+     * @param dateOfBirth
+     * @param dateOfDriverLicense
+     * @return List<Customer>
+     */
+    List<Customer> findCustomersByDateOfBirthAndDriverLIcense (SimpleDateFormat dateOfBirth, SimpleDateFormat dateOfDriverLicense);
+}
--- rentACar/src/main/java/ro/sci/carrental/simulations/SimulateCustomer.java
+++ rentACar/src/main/java/ro/sci/carrental/simulations/SimulateCustomer.java
@@ -1,20 +1,41 @@
 package ro.sci.carrental.simulations;
 
 import ro.sci.carrental.domain.Customer;
-import ro.sci.carrental.repository.CustomerRepository;
 import ro.sci.carrental.repository.CustomerRepositoryImpl;
+import ro.sci.carrental.service.CustomerServiceImpl;
+
+import java.util.List;
 
 /**
  * Created by tudorradovici on 29/05/17.
  */
 public class SimulateCustomer {
-    public void searches(CustomerRepositoryImpl customerRepository){
+    public void searches(CustomerRepositoryImpl customerRepository) {
 
-        System.out.println("List of Customers in Repository");
+        System.out.println ("List of Customers in Repository");
 
-        for (Customer customer : customerRepository.getAll ()){
-            System.out.println (customer.getFirstName ());
-            System.out.println (customer.getLastName ());
+        for (Customer customer : customerRepository.getAll ()) {
+            System.out.println (customer.getFirstName () + customer.getLastName ());
         }
+
+        System.out.println ("End of Customer Repository");
+    }
+
+    CustomerServiceImpl search1 =new CustomerServiceImpl (customerRepository);
+    List<Customer> foundCustomersByFirstName = search1.findCustomersByFirstName ("Tudor");
+
+    for(Customer customer: foundCustomersByFirstName){
+
     }
 }
+
+/**
+ CarServiceImpl search2 = new CarServiceImpl(carRepository);
+ List<Car> foundCarsByMakeAndModel = search2.findCarsByMakeAndModel("Ferrari", "458");
+
+ System.out.println("Search by make and model returns:");
+ for(Car car : foundCarsByMakeAndModel){
+ System.out.println(car.getMake()+" "+car.getModel());
+ }
+
+ System.out.println("End of cars by make and model:");*/
